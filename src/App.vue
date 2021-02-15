32ce5323ea3c97760c7f687b9d123f53ccd371fd
Corentin LE BERRE <leberre.corentin@gmail.com>
2021-02-15 15:25:04 +0100

    trying to fix bug

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,25 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..." />
+        <input 
+        type="text" 
+        class="search-bar" 
+        placeholder="Search..." 
+        v-model="query"
+        @keypress="fetchWeather"
+        />
+      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">Monday 20 January 2020</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°c</div>
+          <div class="weather">Rain</div>
+        </div>
       </div>
     </main>
   </div>
@@ -14,7 +32,23 @@ export default {
   name: 'App',
   data() {
     return {
-      api_key : '6a2b7fb3a578da8807be5ec2b0ba102a'
+      api_key : '6a2b7fb3a578da8807be5ec2b0ba102a',
+      url_base: 'api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {},
+    }
+  },
+  methods: {
+    fetchWeather (e){
+      if (e.key == "Enter") {
+        fetch (`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
     }
   }
 }
@@ -69,5 +103,48 @@ main {
   box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
   background-color: rgba(255,255,255,0.75);
   border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: white;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0,0,0, 0.25);
+}
+
+.location-box .date{
+  color: white;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  color: white;
+  display: inline-block;
+  padding: 10px 25px;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0,0,0,0.25);
+  background-color: rgba(255,255,255,0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: white;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
